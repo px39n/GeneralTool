e63ad5c66f94836dcc48227835b951d8bc493534
px39n <isxzl39@gmail.com>
2019-11-05 16:02:12 +0800

    123

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ import lib.fileIO as IO
 if __name__ == "__main__":
     
     list1=IO.read_csv_to_list("config\dianping_category_to_tag.csv",[6,7])[0]
-    list2=IO.read_csv_to_list("",[6,7])
+    list2=IO.read_csv_to_list("123",[6,7])
     list3=list(set(list1).difference(set(list2)))
