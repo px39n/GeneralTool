7e5dc814788636d2dec934bbf91224e909bb7c56
px39n <isxzl39@gmail.com>
2019-11-08 14:57:23 +0800

    a

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ if __name__ == "__main__":
         item=os.path.splitext(item)[0]
         url=r_url+"/"+item
         list1=IO.read_csv_to_list(url+".csv")
+        list1=[i[1:] for i in list1]
         new_list=[]
         for i in list1:
             i[0]=int(re.findall("eyword/(.+?)/",i[0])[0])
             new_list.append(i)
         new_list.sort()
         new_list.insert(0,["web_url","count","name","type"])
-        IO.write_csv(url+"_1.csv",new_list)
+        IO.write_csv(r_url+"/"+new_list[1][3]+".csv",new_list)
     #print(new_list)
