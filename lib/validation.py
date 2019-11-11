316fc89e3d8c0c8867caa1c96d89263370dae6bf
px39n <isxzl39@gmail.com>
2019-11-11 11:02:30 +0800

    tom

--- lib/validation.py
+++ lib/validation.py
@@ -2,11 +2,9 @@
 
 import fileIO as IO   
 
-
 list1=[]
 list2=[]
 #交集
 list3=[new for new in list1 if new in list2]
-
 #差集
 list3=list(set(list1).difference(set(list2)))
--- main.py
+++ main.py
@@ -6,18 +6,18 @@ import re
 import os
 
 if __name__ == "__main__":
-
-    r_url="C:/Users/admin/Documents/Spider/process"
-    for item in os.listdir(r_url):
-        item=os.path.splitext(item)[0]
-        url=r_url+"/"+item
-        list1=IO.read_csv_to_list(url+".csv")
-        list1=[i[1:] for i in list1]
-        new_list=[]
-        for i in list1:
-            i[0]=int(re.findall("eyword/(.+?)/",i[0])[0])
-            new_list.append(i)
-        new_list.sort()
-        new_list.insert(0,["web_url","count","name","type"])
-        IO.write_csv(r_url+"/"+new_list[1][3]+".csv",new_list)
-    #print(new_list)
+    
+    a=IO.read_csv_to_dict("config/tag.csv",0,[4])
+    b=IO.read_csv_to_list("config\Wayz_Tag_Update.csv")
+    c=[]
+    i=0
+    for item in b:
+        temp=item
+        if a.get(item[1]) and a.get(item[1])[0]:
+            temp[2]=a.get(item[1])[0]
+            i=i+1
+        c.append(temp)
+    IO.write_csv("config\Wayz_Tag_Update_1.csv",c)
+    print("旧标签数量:"+str(len(b)))
+    print("覆盖范围:"+str(i/len(b)))
+    print("未覆盖标签数量:"+str(len(b)-i))
