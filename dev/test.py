b08382fccefa662d35bbabdfa373d1b844f54619
px39n <isxzl39@gmail.com>
2019-11-28 15:48:44 +0800

    s

--- dev/test.py
+++ dev/test.py
@@ -1,6 +1,19 @@
+import pandas as pd
 
-a=(1,2)
+tag="config/test1.csv"
+all="config/test.csv"
+tag=pd.read_csv(tag,",",dtype=object,header=None).values.tolist()  #无标题
+all=pd.read_csv(all,",",dtype=object,header=None).values.tolist()  #无标题
+other=pd.read_csv("config/test1.csv",",",dtype=object,header=None,usecols=[0]).values.tolist()  #无标题
+for record in tag:
+    for count in all:
+        if record[0]==count[0]:
+            record.append(count[1])
+for i in all:
+    if [i[0]] not in other:
+        tag.append(i)
+    
+print(1)
 
-c=list(a)
-x,y=c
-print(x)
+pd.DataFrame(tag).to_csv("config/final.csv",index=False,header=False,encoding="utf_8_sig")
+
--- project/sql_seqence_generator.py
+++ project/sql_seqence_generator.py
@@ -1,3 +1,4 @@
+import pandas as pd
 a=["airport,auto_service,bank,chemist,college,education,energy_station,entertainment,entity,factory,farm,food,foreign_institution,government,hall,health,hospital,hotel,institute,kindergarten,mall,market,office,organization,park,parking,pet,port,residential,road_entrance,scenic,school,service,service_area,shop,sports,station,supermarket,theater,toll,tower,venue"]
 a=a[0].split(",")
 
@@ -7,4 +8,6 @@ for name in a:
     single="union(select '{}',count(*) from {} where tag_names is not null)\n".format(name,name)
     sql=sql+single
 
-
+print(sql)
+url="config/test.txt"
+pd.DataFrame([sql]).to_csv(url,index=False,header=False,encoding="utf_8_sig")
